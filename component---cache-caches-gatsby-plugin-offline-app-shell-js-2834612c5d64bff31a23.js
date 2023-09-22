3726fa1037defeaa86e98376ced8fb7336923939
Praneeth Reddy <penugondapr@gmail.com>
2023-09-22 13:46:45 -0700

    Deploy new portfolio version

---
+++ component---cache-caches-gatsby-plugin-offline-app-shell-js-2834612c5d64bff31a23.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpraneeth_portfolio=self.webpackChunkpraneeth_portfolio||[]).push([[306],{1930:function(e,t,n){var u=n(5318);t.__esModule=!0,t.default=void 0;var r=u(n(5354)),o=u(n(7294)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,r.default)(t,e),t.prototype.render=function(){return o.default.createElement(o.default.Fragment,null)},t}(o.default.Component),l=a;t.default=l}}]);
+//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-2834612c5d64bff31a23.js.map
